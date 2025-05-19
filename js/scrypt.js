481d9ac763c35ffb8516c21eaced2cfec49c518b
Zimith <lucas.ochoa@itscordoba.edu.ar>
2025-05-18 21:37:02 -0300

    primer js

---
+++ js/scrypt.js
@@ -0,0 +1,17 @@
+let nombre = "ana";
+const edad= 25;
+
+function saludar(nombre){
+    return "Hola" + nombre;
+}
+
+if (edad >= 18){
+    console.log("Es mayor de edad");
+}else{
+    console.log("No es mayor");
+}
+
+function mostraralerta() {
+    alert("Hola desde java");
+  };
+  
